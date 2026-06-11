502ffd6c9af40886f0382061f71d5592f6a09405
sreesanthtshibu <sreesanthtshibu@gmail.com>
2026-06-11 15:13:11 +0530

    Implement FizzBuzz solution in fizzbuzz.py

---
+++ DAY3/fizzbuzz.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+
+        answer = []
+
+        for i in range(1, n + 1):
+
+            if i % 3 == 0 and i % 5 == 0:
+                answer.append("FizzBuzz")
+
+            elif i % 3 == 0:
+                answer.append("Fizz")
+
+            elif i % 5 == 0:
+                answer.append("Buzz")
+
+            else:
+                answer.append(str(i))
+
+        return answer
+obj = Solution()
+s= obj.fizzBuzz(10)
+print(s)
